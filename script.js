f250a215d4d37f9f27b6c98639661f9e11d58ce8
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-05-25 15:43:13 +0500

    ssalom

---
+++ script.js
@@ -0,0 +1,10 @@
+const a =prompt("sizninig ismingiz nima");
+const b =prompt("sizninig familyangiz nima");
+const c =prompt("siz necha yoshtasiz");
+const d =prompt("siz qayerda yashaysiz");
+const e =prompt("sizninig yoqtirgan faning giz");
+ console.log(a);
+ console.log(b);
+ console.log(c);
+ console.log(d);
+ console.log(e);
